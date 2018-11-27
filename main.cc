8d670da311e3326fee01d309a1d027fe0cde8252
Denis Gavrielov <dg3718@point39.doc.ic.ac.uk>
2018-11-27 21:18:21 +0000

    start working on skeleton of consumer thread

--- main.cc
+++ main.cc
@@ -11,11 +11,18 @@ void *consumer (void *id);
 
 struct job {
 	int job_id;
+	int id; // for producer and consumer
 	int duration;
+	int* queue;
+	int* tail;
+	int* head;
+	int queue_size;
 };
 
-// need 3 semaphore
+// need 3 semaphores
 int sem = sem_create(SEM_KEY, 3); 
+/* note: could also be put into main and have pointer pointing 
+to the sem (?)*/
 
 int main (int argc, char **argv) {
 
@@ -31,18 +38,57 @@ int main (int argc, char **argv) {
 	int producers = 10;
 	int consumers = 10;
 
-	// mutex, buffer not full, buffer not empty
-	sem_init(sem, 0, 1); // mutex
-	sem_init(sem, 1, queue_size); // queue not full
-	sem_init(sem, 2, 0); // queue not empty
+	// check if init worked
+	int returns = sem_init(sem, 0, 1); // mutex
+	if (returns)
+		cout << "SEMAPHORE NOT CREATED" << endl;
+	returns = sem_init(sem, 1, queue_size); // queue not full
+	if (returns)
+		cout << "SEMAPHORE NOT CREATED" << endl;
+	returns = sem_init(sem, 2, 0); // queue not empty
+	if (returns)
+		cout << "SEMAPHORE NOT CREATED" << endl;
 
-	pthread_t producerid;
+	// queue
+	int* queue = new int[queue_size];
+	int* tail = new int(0); // points to where consumer takes out
+	int* head = new int(0); // pointer to where producer pushes in
+
+
+	pthread_t producerid[producers];
+	pthread_t consumerid[consumers];
+
 	job next_job;
-	next_job.duration = 6; // should be random number
 
-	pthread_create (&producerid, NULL, producer, (void *) &next_job);
+	next_job.queue = queue;
+	next_job.tail = tail;
+	next_job.head = head;
+	next_job.queue_size = queue_size;
 
-  	pthread_join (producerid, NULL);
+	/* NOTE: potential issue: we will always pass the same struct, so whatever
+	 * change we make to the struct will be global for all the other structs.
+	 * Thus we need to make sure that I actually pass different structs every
+	 * time. E.g. by creating new structs within the for loop and changing the
+	 * value of id.
+	 * solution: create base_struct before the loop with all the variables
+	 * initialised. Within the loop create a new temp_struct ON THE HEAP, that copies
+	 * everything from base_struct, but changes the id. then we pass this
+	 * temp_struct instead so we are sure that every process will get it
+	 * individual struct. 
+	 * Maybe can do this more memory lightweight by having producer_id (need to split)
+	 * as a pointer to something on the heap that every producer will read and
+	 * then increment by one (could use semaphore to block) and then every
+	 * producer have their own id. 
+	*/
+	pthread_create (&producerid[0], NULL, producer, (void *) &next_job);
+
+	// 0 will be i in the for loop
+	pthread_create (&consumerid[0], NULL, consumer, (void *) &next_job);
+
+  	pthread_join (producerid[0], NULL);
+	pthread_join (consumerid[0], NULL);
+
+	sem_close(sem); // alternatively/manually ipcrm -s [number from ipcs]
 
   	return 0; 
 }
@@ -51,7 +97,7 @@ void *producer (void *next_job) {
 	/*	
 	Initialise parameters 
 	add jobs into the jobs (randomly every 1-5 seconds) 
-	If job is taken by consumer -> can ad another one with same id (so basically
+	If job is taken by consumer -> can add another one with same id (so basically
 	if we free up the space) 
 		* duration of job: 1-10s
 	
@@ -65,13 +111,33 @@ void *producer (void *next_job) {
 	*/
 
 	job *current_job = (job *) next_job;
+	int *head = current_job->head;
 
-	cout << "Parameter = " << current_job->duration << endl;
+	// produce duration and ID of the job before we put it into the queue we
+	// sleep.
+
+	int duration = 9; // random function. 
+	
+	sem_wait(sem, 1); // down on space
+	
+	// We check if there is reduce the space first(1), then only we would
+	// activate the mutex(0)
+	sem_wait(sem, 0);
+	cout << "mutex down completed" << endl;
+	
+	current_job->job_id = *head; // maybe change to plus one 
+	current_job->queue[*head] = duration;
+	*(current_job->head) = (*head + 1) % (current_job->queue_size) - 1;
+	
+	sem_signal(sem, 0);
+	cout << "mutex up completed" << endl;
+	// we put the mutex(0) up, then we increase the item (2)
+	sem_signal(sem, 2);
 
 	pthread_exit(0);
 }
 
-void *consumer (void *id) {
+void *consumer (void *next_job) {
 	/*
 	* take job from circular queue -> 'sleep' for duration.
 	* block if queue is empty. If blocked for 20s -> quit.
@@ -82,6 +148,10 @@ void *consumer (void *id) {
 	* if no jobs -> wait for 20s and then quit.  ( NOTE: implement this delay as
 	* part of a semaphore ). 
 	*/
+
+	job *current_job = (job *) next_job;
+	
+
 
 	pthread_exit (0);
 
